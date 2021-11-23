83602c299c71a0fdd23d3bd769959d01c33508ba
amitha6 <56950230+amitha6@users.noreply.github.com>
2021-11-23 11:08:35 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+import unittest
+from main import divide
+
+class testdivide(unittest.TestCase): 
+  def testdivide1(self):
+    self.assertEqual(divide (10, 5), 2)
+    
+ if __name__ == '__main__':
+    unittest.main()
