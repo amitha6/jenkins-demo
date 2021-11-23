5ee700b4b93f43b1713d489cdb831ffdaefea2aa
amitha6 <56950230+amitha6@users.noreply.github.com>
2021-11-24 03:04:04 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from main import divide
 
 class testdivide(unittest.TestCase):
   def testdivide1(self):
-    self.assertEqual(divide (10, 5), 4)
+    self.assertEqual(divide (10, 5), 2)
     
 if __name__ == '__main__':
   unittest.main()
